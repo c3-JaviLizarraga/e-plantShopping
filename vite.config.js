0dd78d0a607a50341220abe02c5cc28dea25f39c
c3-JaviLizarraga <javier.lizarraga@c3.ai>
2025-10-01 20:40:11 -0700

    FinalProject

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/c3-JaviLizarraga/e-plantShopping",
   plugins: [react()],
 })
